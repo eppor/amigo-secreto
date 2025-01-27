921360d5de595fab6258391e0e8f3dcfea3b1214
eppor <emiliaprzybyszewska@gmail.com>
2025-01-27 19:31:47 -0300

    Captura o valor do campo da entrada e valida se o campo nao esta vazio

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Inicie declarando uma variável do tipo array, que armazenará os nomes dos amigos inseridos.
+//let amigos[];
+// let nomeDoAmigo = document.getElementById('amigo');
+// console.log(nomeDoAmigo);
+
+function adicionarAmigo() {
+    let nomeDoAmigo = document.querySelector('input').value;
+        if (nomeDoAmigo == '') {
+        alert("Por favor, insira um nome.");
+        return false;
+    }   else {
+        console.log(nomeDoAmigo);
+    }
+    }
